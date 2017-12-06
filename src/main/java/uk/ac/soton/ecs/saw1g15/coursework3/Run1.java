9f50a33beba807a4872b7c28b2f4abe7030eda5f
SWilliams <scottwill@tesco.net>
2017-12-06 01:35:00 +0000

    pom etc

--- src/main/java/uk/ac/soton/ecs/saw1g15/coursework3/Run1.java
+++ src/main/java/uk/ac/soton/ecs/saw1g15/coursework3/Run1.java
@@ -1,5 +1,7 @@
 package uk.ac.soton.ecs.saw1g15.coursework3;
 
+import java.util.ArrayList;
+
 import org.openimaj.data.dataset.VFSGroupDataset;
 import org.openimaj.data.dataset.VFSListDataset;
 import org.openimaj.image.DisplayUtilities;
@@ -17,6 +19,9 @@ public class Run1 {
     		VFSGroupDataset<FImage> training = 
     				new VFSGroupDataset<FImage>("zip:http://comp3204.ecs.soton.ac.uk/cw/training.zip", ImageUtilities.FIMAGE_READER);
     		
+    		// Plotted Vectors
+    		ArrayList<float[]> graph = new ArrayList<float[]>();
+    		
     		// For every image in the training set...
     		for (FImage image : training) {
     			// Crop image to a square
@@ -25,11 +30,18 @@ public class Run1 {
     			// Resize the image to 16x16
     			FImage resized = image.process(new ResizeProcessor(16, 16, false));
     			
-    			// Create vector by concatenating each image row
+    			float[] imageVector = new float[resized.getRows() * resized.getCols()];
     			
+    			// Create vector by concatenating each image row
+    			int counter = 0;
+    			for (int i = 0; i < resized.getRows(); i++) {
+    				for (int j = 0; j < resized.getCols(); j++) {
+    					imageVector[counter] = resized.getPixel(i, j);
+    				}
+    			}   			
     			
     			// Plot the vector
-    			
+    			graph.add(imageVector);
     			
     		}
     		
